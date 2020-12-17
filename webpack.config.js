37edd72c1a3c0d5a6b024a1dc3bad8105bb30377
Dinesh Kathuria <dinesh.kathuria@paytm.com>
2020-12-18 00:15:01 +0530

    Added v0.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
+var GitRevisionPlugin = require('git-revision-webpack-plugin')
+const gitRevisionPlugin = new GitRevisionPlugin();
+
+console.log('Git Version', gitRevisionPlugin.version());
+
 module.exports = {
     plugins: [
-    
+      new GitRevisionPlugin()
     ]
-};
-  
+  }
