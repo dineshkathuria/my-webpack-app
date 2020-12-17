97ebfb9dcc0ae19b5aa3c0fe58589110e68e9270
Dinesh Kathuria <dinesh.kathuria@paytm.com>
2020-12-18 00:22:43 +0530

    Added console

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,11 @@
 var GitRevisionPlugin = require('git-revision-webpack-plugin')
 const gitRevisionPlugin = new GitRevisionPlugin();
 
-console.log('Git Version', gitRevisionPlugin.version());
+console.log('p                                                                               p');
+console.log('================== Git Version ==================', gitRevisionPlugin.version());
+console.log('p                                                                               p');
+
+console.log('p                                                                               p');
 
 module.exports = {
     plugins: [
